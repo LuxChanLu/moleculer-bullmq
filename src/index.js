ee3935538fbcdc9320fa7948f7b5d60d48f40815
Hugo Meyronneinc <hugomeyronneinc@gmail.com>
2020-01-30 05:25:49 +0100

    Add basic functions

--- src/index.js
+++ src/index.js
@@ -6,6 +6,39 @@
 
 'use strict'
 
-module.exports = {
+const { Context } = require('moleculer')
+const { Worker, Queue } = require('bullmq')
 
+module.exports = {
+  settings: {
+    bullmq: {
+      worker: {}
+    }
+  },
+  created() {
+    this.$queues = Object.entries(this.schema.actions).filter(([, { queue }]) => queue).map(([name]) => name)
+    this.$queueResolved = {}
+    if (this.$queues.length > 0) {
+      this.$worker = new Worker(this.name, async job => {
+        if (this.$queues.includes(job.name)) {
+          const { params, meta } = job.data
+          const ctx = Context.create(this.broker, undefined, params, { meta })
+          ctx.locals.job = job
+          await ctx.call(`${this.name}.${job.name}`, params)
+        }
+      }, { ...this.settings.bullmq.worker, connection: this.broker.cacher.client })
+      this.$worker.on('waiting', ({ id, name }) => this.broker.emit(`${this.name}.${name}.waiting`, { id }))
+      this.$worker.on('drained', ({ id, name }) => this.broker.emit(`${this.name}.${name}.drained`, { id }))
+      this.$worker.on('completed', ({ id, name }) => this.broker.emit(`${this.name}.${name}.completed`, { id }))
+      this.$worker.on('failed', ({ id, name }) => this.broker.emit(`${this.name}.${name}.failed`, { id }))
+      this.$worker.on('progress', ({ id, name }, progress) => this.broker.emit(`${this.name}.${name}.progress`, { id, progress }))
+      this.$worker.on('completed', ({ id, name }) => this.broker.emit(`${this.name}.${name}.completed`, { id }))
+    }
+  },
+  methods: {
+    queue(name, action, params) {
+      const queue = this.$queueResolved[name] || (this.$queueResolved[name] = new Queue(this.name, { connection: this.broker.cacher.client }))
+      return queue.add(action, { params, meta: this.currentContext.meta })
+    }
+  }
 }
