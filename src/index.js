3600736573374a68083a8262db73698a0117daa0
Hugo Meyronneinc <hugomeyronneinc@gmail.com>
2020-02-03 03:24:13 +0100

    Test OK + Fix GH actions

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
 'use strict'
 
 const { Context } = require('moleculer')
-const { Worker, Queue } = require('bullmq')
+const { Worker, Queue, QueueEvents } = require('bullmq')
 
 module.exports = {
   settings: {
@@ -15,44 +15,70 @@ module.exports = {
       worker: {}
     }
   },
+  hooks: {
+    before: {
+      '*': '$populateJob'
+    }
+  },
   created() {
-    this.$queues = Object.entries(this.schema.actions).filter(([, { queue }]) => queue).map(([name]) => name)
+    this.$queues = Object.entries(this.schema.actions || {}).filter(([, { queue }]) => queue).map(([name]) => name)
     this.$queueResolved = {}
+  },
+  started() {
     if (this.$queues.length > 0) {
       this.$worker = new Worker(this.name, async job => {
-        if (this.$queues.includes(job.name)) {
-          const { params, meta } = job.data
-          if (!this.broker.registry.hasService(this.name)) {
-            await this.broker.waitForServices(this.name)
-          }
-          const ctx = Context.create(this.broker, undefined, params, { meta, timeout: 0 })
-          ctx.locals.job = job
-          await ctx.call(`${this.name}.${job.name}`, params)
-        }
-      }, { ...this.settings.bullmq.worker, connection: this.broker.cacher.client })
-      const evt = (name, type, params) => {
-        const event = [name, type].filter(Boolean).join('.')
-        this.broker.emit(`${this.name}.${event}`, params)
-        this.broker.broadcastLocal(event, params)
-      }
-      this.$worker.on('waiting', ({ id, name }) => evt(name, 'waiting', { id }))
-      this.$worker.on('drained', () => evt('drained'))
-      this.$worker.on('failed', ({ id, name }) => evt(name, 'failed', { id }))
-      this.$worker.on('progress', ({ id, name }, progress) => evt(name, 'progress', { id, progress }))
-      this.$worker.on('completed', ({ id, name }) => evt(name, 'completed', { id }))
+        const { params, meta } = job.data
+        meta.job = { id: job.id, queue: this.name }
+        return Context.create(this.broker, undefined, params, { meta, timeout: 0 }).call(`${this.name}.${job.name}`, params)
+      }, { ...this.settings.bullmq.worker, client: this.broker.cacher.client })
+      this.$events = new QueueEvents(this.name, { client: this.broker.cacher.client })
+      this.$events.on('active', ({ jobId }) => this.$transformEvent(jobId, 'active'))
+      this.$events.on('removed', ({ jobId }) => this.$transformEvent(jobId, 'removed'))
+      this.$events.on('progress', ({ jobId, data }) => this.$transformEvent(jobId, 'progress', { progress: data }))
+      this.$events.on('failed', ({ jobId }) => this.$transformEvent(jobId, 'failed'))
+      this.$events.on('error', ({ jobId }) => this.$transformEvent(jobId, 'error'))
+      this.$events.on('completed', ({ jobId }) => this.$transformEvent(jobId, 'completed'))
+      this.$events.on('drained', () => this.$transformEvent('drained'))
+      this.$events.on('paused', () => this.$transformEvent('paused'))
+      this.$events.on('resumed', () => this.$transformEvent('resumed'))
+      this.$events.on('cleaned', (jobs, type) => this.$transformEvent(null, 'cleaned', { jobs: jobs.map(({ id }) => id), type }))
     }
+  },
+  async stopped() {
+    if (this.$worker) {
+      await this.$worker.close()
+    }
+    if (this.$events) {
+      await this.$events.close()
+    }
+    await Promise.all(Object.values(this.$queueResolved).map(queue => queue.close()))
   },
   methods: {
     $resolve(name) {
-      return this.$queueResolved[name] || (this.$queueResolved[name] = new Queue(this.name, { connection: this.broker.cacher.client }))
-    },
-    queue(name, action, params) {
-      if (arguments.length <= 2) {
-        params = action
-        action = name
-        name = this.name
+      if (!this.$queueResolved[name]) {
+        this.$queueResolved[name] = new Queue(name, { client: this.broker.cacher.client })
       }
-      return this.$resolve(name).add(action, { params, meta: this.currentContext.meta })
+      return this.$queueResolved[name]
+    },
+    pause(name) {
+      if (name) {
+        return this.$resolve(name).pause()
+      } else if (this.$worker) {
+        return this.$worker.pause()
+      }
+    },
+    resume(name) {
+      if (name) {
+        return this.$resolve(name).resume()
+      } else if (this.$worker) {
+        return this.$worker.resume()
+      }
+    },
+    queue(name, action, params, options) {
+      return this.$resolve(name).add(action, { params, meta: this.currentContext.meta }, options)
+    },
+    localQueue(action, params, options) {
+      return this.queue(this.name, action, params, options)
     },
     job(name, id) {
       if (arguments.length === 1) {
@@ -60,6 +86,24 @@ module.exports = {
         name = this.name
       }
       return this.$resolve(name).getJob(id)
+    },
+    async $populateJob (ctx) {
+      ctx.locals.job = ctx.meta && ctx.meta.job ? await this.job(ctx.meta.job.queue, ctx.meta.job.id) : undefined
+    },
+    async $transformEvent (id, type, params) {
+      const ctx = this.currentContext
+      const event = arguments.length === 1 ? [id] : [type]
+      if (arguments.length >= 2 && id) {
+        const job = await this.job(id)
+        if (job && job.name) {
+          event.unshift(job.name)
+        }
+        params = params || {}
+        params.id = id
+      }
+      const name = event.join('.')
+      ctx.emit(`${this.name}.${name}`, params)
+      ctx.emit(name, params, this.name)
     }
   }
 }
--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -6,21 +6,123 @@
 
 'use strict'
 
-const { ServiceBroker } = require('moleculer')
-
+const { ServiceBroker, Context } = require('moleculer')
+const RedisMock = require('redis-mock')
+const WaitForExpect = require('wait-for-expect')
 const BullMqMixin = require('../../src/index.js')
 
-describe('Basic test', () => {
-  const broker = new ServiceBroker({ logger: false })
-  const service = broker.createService({
-    name: 'bullmq',
-    mixins: [BullMqMixin]
+describe('Mixin', () => {
+  const broker = new ServiceBroker({
+    logger: false,
+    cacher: {
+      type: 'Redis',
+      options: {
+        redis: RedisMock.createClient(),
+        lock: { ttl: 10 }
+      }
+    }
   })
+  const service = broker.createService({
+    name: 'jobs',
+    mixins: [BullMqMixin],
+    settings: {
+      bullmq: {
+        worker: { concurrency: 1 }
+      }
+    },
+    actions: {
+      resize: {
+        queue: true,
+        async handler(ctx) {
+          const { width, height } = ctx.params
+          const { bucket } = ctx.meta
+          if (ctx.locals.job) {
+            ctx.locals.job.updateProgress(100)
+            return { bucket, size: width * height, job: ctx.locals.job.id }
+          }
+        }
+      },
+      payment: {
+        queue: true,
+        params: { amount: 'number' },
+        async handler()  {
+          throw new Error('Your too poor for this payment')
+        }
+      },
+      'report.generate': {
+        async handler() {
+          const job = await this.localQueue('resize')
+          await job.remove()
+          return job
+        }
+      }
+    }
+  })
+  const ctx = service.currentContext = Context.create(broker, undefined, undefined, { meta: { bucket: 'NGNLS2' } })
+  const emitSpy = jest.spyOn(ctx, 'emit')
+  const scheduler = broker.createService({ name: 'scheduler', mixins: [BullMqMixin] }) // Try without actions
+
+  const expectJobEvent = (name, params) => {
+    expect(emitSpy.mock.calls).toContainEqual([`${service.name}.${name}`, params])
+    expect(emitSpy.mock.calls).toContainEqual([name, params, service.name])
+  }
 
   beforeAll(() => broker.start())
   afterAll(() => broker.stop())
 
-  it('basic it', async () => {
-    expect(service).toBeDefined()
+  it('should have a bull worker', () => expect(service.$worker).toBeDefined())
+
+  it('should queue a successful job', async () => {
+    const job = await service.localQueue('resize', { width: 42, height: 42 })
+    await WaitForExpect(async () => {
+      expect(emitSpy).toHaveBeenCalledTimes(8)
+
+      expectJobEvent('resize.active', { id: job.id })
+      expectJobEvent('resize.progress', { id: job.id, progress: 100 })
+      expectJobEvent('resize.completed', { id: job.id })
+      expectJobEvent('drained', undefined)
+
+      const { returnvalue, progress } = await service.job(job.id)
+      expect(returnvalue).toStrictEqual({ bucket: 'NGNLS2', size: 1764, job: job.id }) // This confirm the meta, params & locals has been passed to the job
+      expect(progress).toBe(100)
+    })
+  })
+
+  it('should queue a failed job', async () => {
+    emitSpy.mockClear()
+    const jobs = [await service.queue(service.name, 'payment', { amount: 2000 }, { priority: 200 }), await service.queue(service.name, 'payment')]
+    await WaitForExpect(async () => {
+      expect(emitSpy).toHaveBeenCalledTimes(10)
+
+      expectJobEvent('payment.active', { id: jobs[0].id })
+      expectJobEvent('payment.failed', { id: jobs[0].id })
+
+      expectJobEvent('payment.active', { id: jobs[1].id })
+      expectJobEvent('payment.failed', { id: jobs[1].id })
+
+      expectJobEvent('drained', undefined)
+
+      const errors = [await service.job(service.name, jobs[0].id), await service.job(service.name, jobs[1].id)]
+      expect(errors[0].failedReason).toBe('Your too poor for this payment')
+      expect(errors[1].failedReason).toBe('Parameters validation error!')
+    })
+  })
+
+  it('should emit misc events', async () => {
+    emitSpy.mockClear()
+    const job = await ctx.call('jobs.report.generate')
+    await service.pause()
+    await service.resume()
+    await scheduler.pause(service.name)
+    await scheduler.resume(service.name)
+    await scheduler.pause()
+    await scheduler.resume()
+    await WaitForExpect(() => {
+      expect(emitSpy).toHaveBeenCalledTimes(12)
+
+      expectJobEvent('removed', { id: job.id })
+      expectJobEvent('paused')
+      expectJobEvent( 'resumed')
+    })
   })
 })
